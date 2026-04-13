1810a17d992c28c54037996b2724e2f3e7254f12
Uami-11 <nirwan.m.9618@gmail.com>
2026-04-13 13:02:59 +0545

    add: make structs that can hold the fields from theme.txt

---
+++ parser/types.go
@@ -0,0 +1,73 @@
+// Package parser looks thorugh the theme.txt and parses it and looks for errors
+package parser
+
+type Theme struct {
+	// ---- desktop values
+	DesktopImage string
+	DesktopColor string
+
+	// --- terminal values
+	TerminalBox    string
+	TerminalTop    string
+	TerminalLeft   string
+	TerminalWidth  string
+	TerminalHeight string
+	TerminalBorder string
+	TerminalFont   string
+
+	TitleText  string
+	Components []Component
+}
+
+type ComponentType string
+
+const (
+	ComponentLabel       ComponentType = "label"
+	ComponentBootMenu    ComponentType = "boot_menu"
+	ComponentProgressBar ComponentType = "progress_bar"
+	ComponentVBox        ComponentType = "vbox"
+	ComponentHBox        ComponentType = "hbox"
+	ComponentImage       ComponentType = "image"
+)
+
+type Component struct {
+	Type ComponentType
+
+	// line number in the theme.txt
+	Line int
+
+	// Position
+	Left   string
+	Top    string
+	Width  string
+	Height string
+
+	// Label fields
+	Text  string
+	Font  string
+	Color string
+	Align string
+	ID    string
+
+	// Boot menu fields
+	ItemFont                string
+	ItemColor               string
+	SelectedItemColor       string
+	ItemHeight              string
+	ItemPadding             string
+	ItemSpacing             string
+	ItemPixmapStyle         string
+	SelectedItemPixmapStyle string
+	IconWidth               string
+	IconHeight              string
+	ItemIconSpace           string
+
+	// Progress bar fields
+	FgColor     string
+	BgColor     string
+	BorderColor string
+
+	File string
+
+	Children []Component
+}
